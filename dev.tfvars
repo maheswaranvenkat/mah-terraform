a880dc5aec00d828959f279ba0f02a57f84f8561
Hari Kammana <hari.kammana@gmail.com>
2019-07-23 14:58:03 +0530

    Terraform Course

--- dev.tfvars
+++ dev.tfvars
@@ -1,1 +1,2 @@
-vpc_cidr = "10.30.0.0/16"
+vpc_cidr           = "10.20.0.0/16"
+s3_web_bucket_name = "javahome-udemy-dev"
--- providers.tf
+++ providers.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket         = "javahome-tf-1234567"
+    bucket         = "javahome-tf-12345678"
     key            = "terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "javahome-tf"
